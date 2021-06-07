b2e953fefe801ae23019e16e8d840c3936bd73cc
defenceplox <defenceplox@gmail.com>
2021-06-07 18:53:00 +0100

    code cleanup

--- assets/js/scroll.js
+++
@@ -1,8 +0,0 @@
-<script>
- $('.links').click(function(e){ 
-	 $('html, body').animate({
-   scrollTop: $( $.attr(this, 'href') ).offset().top - $('.nav').height()
-	 }, 1000);
-  return false;
- });
-</script>
